be6449397355e61cc7c3408a475ba05145adff11
Yuri van Warmerdam <yurivwarmerdam@gmail.com>
2025-01-18 00:36:03 +0100

    working on making tilemap

--- pytmx_experiment.py
+++ pytmx_experiment.py
@@ -6,21 +6,30 @@ from pytmx.util_pygame import load_pygame
 screen = pg.display.set_mode((1280, 960))
 display = pg.Surface((640, 480))
 
-tmx_data = load_pygame("art/tmx/field.tmx")
-tile_group=pg.sprite.Group()
+# self.tmx_data = load_pygame("art/tmx/field.tmx")
+# tile_group = pg.sprite.Group()
+
 
 class Tile(pg.sprite.Sprite):
-    def __init__(self, pos, surf, groups):
-        pg.sprite.Sprite.__init__(self)
-        super().__init__(groups)
+    def __init__(self, pos, surf, *groups):
+        # pg.sprite.Sprite.__init__(self)
+        super().__init__(*groups)
         self.image = surf
         self.rect = self.image.get_rect(topleft=pos)
 
-for layer in tmx_data.visible_layers:
-    if hasattr(layer,"data"):
-        for x,y,surf in layer.tiles():
-            pos=(x*tmx_data.tilewidth,y*tmx_data.tileheight)
-            Tile(pos,surf,tile_group)
+class Tilemap():
+    def __init__(self,tmx_file):
+        self.tmx_data=load_pygame(tmx_file)
+        pass
+
+    for layer in self.tmx_data.visible_layers:
+
+        if hasattr(layer, "data"):
+            for x, y, surf in layer.tiles():
+                pos = (x * self.tmx_data.tilewidth, y * self.tmx_data.tileheight)
+                Tile(pos, surf, self)
+
+
 
 while True:
     for event in pg.event.get():
@@ -30,7 +39,6 @@ while True:
 
     display.fill((14, 64, 128))
 
-    pg.draw.rect(display, (40, 40, 40), pg.Rect(500, 50, 20, 20))
     tile_group.draw(display)
 
     screen.blit(pg.transform.scale(display, screen.get_size()), (0, 0))
